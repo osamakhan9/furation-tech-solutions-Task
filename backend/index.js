ec7f9948eefad1c2cb5446fbf907b5ab1b30dc34
mohammad osama <okosama06@gmail.com>
2023-07-06 15:45:48 +0530

    utils page  backend done

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,27 @@
 const express=require("express");
-
+const colors=require("colors")
  const connecttion = require("./config/db");
-
+ const { notFound, errorHandler }=require("./middlewares/errorMiddleware")
 
 require("dotenv").config()
 const port=process.env.PORT||8080
 const app=express();
 app.use(express.json())
 const cors = require('cors');
-
+const authRouter = require("./routes/User");
+const busRouter = require("./routes/busRoute");
+const cartRouter = require("./routes/cartRoute");
+const bookedRouter = require("./routes/bookedRoute");
 app.use(cors());
 app.get("/",(req,res)=>{
     res.send("Welcome to server")
 })
-
+app.use("/api/auth",authRouter)
+app.use("/api/bus",busRouter)
+app.use("/api/bus/cart",cartRouter)
+app.use("/api/bus/book",bookedRouter)
+app.use(notFound)
+app.use(errorHandler)
 app.listen(port,async()=>{
     try {
         await connecttion
